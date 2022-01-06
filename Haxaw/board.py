0147b58bf932a11e62309e6387be7eef59401b4a
Sarthak <sarthakbh321@gmail.com>
2022-01-06 23:09:28 +0530

    feat(Haxaw): dataset generation

--- Haxaw/board.py
+++ Haxaw/board.py
@@ -1,6 +1,35 @@
 import chess
-
+from chess.svg import board
+import numpy as np
 
 class Board():
     def __init__(self, fen=chess.STARTING_FEN):
         self.board = chess.Board(fen)
+
+    def convert_to_representation(self):
+        
+        encoding_dict = {"K": 1, "k": 5, "Q": 9, "q": 10, "R": 11, "r": 12, \
+                         "B": 13, "b": 14, "N": 15, "n": 16, "P": 17, "p": 18}
+
+
+        board_state = np.zeros(64, dtype=np.uint8)
+
+        for i in range(64):
+            piece = self.board.piece_at(i)
+
+            if(piece != None):
+                p_symbol = piece.symbol()
+                board_state[i] = encoding_dict[p_symbol]
+
+                if(p_symbol == "K" or p_symbol == "k"):
+                    if(self.board.has_kingside_castling_rights(piece.color) and self.board.has_queenside_castling_rights(piece.color)):
+                        board_state[i] += 3
+                    elif(self.board.has_kingside_castling_rights(piece.color)):
+                        board_state[i] += 1
+                    elif(self.board.has_queenside_castling_rights(piece.color)):
+                        board_state[i] += 2
+
+        if(self.board.ep_square != None):
+            board_state[self.board.ep_square] = 19
+
+        return board_state
--- Haxaw/generate_dataset.py
+++ Haxaw/generate_dataset.py
@@ -1,40 +1,63 @@
 import pandas as pd
 import chess.pgn
 from board import Board
+import numpy as np
+
 
 pgn = open("./data/data.pgn")
 evaluation = pd.read_csv("./data/stockfish.csv")
 
-def generate():
-
+def generate(limit):
+    training_features, training_labels = [], []
     gameIndex = 0
+    positions_parsed = 0
 
     while(True):
+
+        if(positions_parsed > limit):
+            break
+
         game = chess.pgn.read_game(pgn)
         
         move_scores = evaluation["MoveScores"][gameIndex]
-        move_scores = list(map(int, move_scores.split()))
+        move_scores = move_scores.split()
 
         if game is None:
             break
         
-        print("Parsing game #%d" % gameIndex)
         gs = Board()
 
         for move_index, move in enumerate(game.mainline_moves()):
             centipawn_valuation = move_scores[move_index]
+
+            try:
+                centipawn_valuation = int(centipawn_valuation)
+            except Exception:
+                centipawn_valuation = 0
+
             gs.board.push(move)
-            print(move ,game.headers["Result"], centipawn_valuation)
-            print(gs.board)
+            encoded = gs.convert_to_representation()
+            
+            training_features.append(encoded)
+            training_labels.append(centipawn_valuation)
+            positions_parsed += 1
 
         gameIndex += 1
-        exit(0)
+        print("Parsed: %d games and %d positions" % (gameIndex, positions_parsed))
+                
 
     print("Parsed all games")
+    training_features = np.array(training_features)
+    training_labels = np.array(training_labels)
+
+    return training_labels, training_features
         
 
 
 
 if __name__ == "__main__":
     print("Generating dataset...")
-    generate()
+    X, Y = generate(50000)
+    
+    np.savez("dataset/dataset_50k.npz", X, Y)
+
