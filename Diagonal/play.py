a80d42919c23fc31b390e08389a27f8f9c99574f
Sarthak <sarthakbh321@gmail.com>
2022-01-07 23:20:42 +0530

    feat(both): can now play a game with itself

--- Diagonal/play.py
+++ Diagonal/play.py
@@ -3,4 +3,14 @@ from Haxaw.board import Board
 
 game_state = Board()
 
-print(game_state.get_best_moves()[:5])
+while(game_state.board.outcome() == None):
+    moves = game_state.get_best_moves()
+
+    val, move = moves[0]
+
+    game_state.board.push(move)
+    print(game_state.board)
+    print("----------")
+
+print(game_state.board.result())
+
